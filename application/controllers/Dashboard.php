f580b6650d09065ac4738ec369beda5ce68d3062
Ridwanizer <ridwanhanantoaji@gmail.com>
2020-05-29 14:44:37 +0700

    Frontend
    Alhamdulillah bisa

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+    function __construct()
+	{
+		parent::__construct();
+		$this->load->model('M_dashboard');
+		
+    }
+    public function index()
+	{
+		// $this->M_login->keamananLogout();
+		
+		$data = array(
+			'title' => 'Dashboard'
+		);
+		$this->load->view('dashboard');
+
+		
+	}
+}
---
+++ application/models/M_dashboard.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_dashboard extends CI_Model
+{
+    
+}
